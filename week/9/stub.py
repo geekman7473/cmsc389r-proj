a98651f768e5e81abb9dacdaad3bd4e6682b43f0
Justin Becker <geekman7473@gmail.com>
2019-11-12 18:11:22 -0500

    hw9 finished

--- week/9/stub.py
+++ week/9/stub.py
@@ -3,15 +3,27 @@
 import sys
 import struct
 
-
 # You can use this method to exit on failure conditions.
 def bork(msg):
     sys.exit(msg)
 
-
 # Some constants. You shouldn't need to change these.
 MAGIC = 0x8BADF00D
 VERSION = 1
+
+SECTION_ASCII = 1
+SECTION_UTF8 = 2
+SECTION_WORDS = 3
+SECTION_DWORDS = 4
+SECTION_DOUBLES = 5
+SECTION_COORD = 6
+SECTION_REFERENCE = 7
+SECTION_PNG = 8
+SECTION_GIF87 = 9
+SECTION_GIF89 = 10
+
+section_to_name_map = {1:"ASCII", 2:"UTF-8", 3:"WORDS", 4:"DWORDS", 5:"DOUBLES", 6:"COORD", 7:"REFERENCE", 8:"PNG", 9:"GIF87", 10:"GIF89"}
+image_format_offset_table = {8:[137, 80, 78, 71, 13, 10, 26, 10], 9:[47, 49, 46, 38, 37, 61], 10:[47, 49, 46, 38, 39, 61]}
 
 if len(sys.argv) < 2:
     sys.exit("Usage: python stub.py input_file.fpff")
@@ -21,10 +33,10 @@ if len(sys.argv) < 2:
 with open(sys.argv[1], 'rb') as fpff:
     data = fpff.read()
 
-# Hint: struct.unpack will be VERY useful.
-# Hint: you might find it easier to use an index/offset variable than
-# hardcoding ranges like 0:8
 magic, version = struct.unpack("<LL", data[0:8])
+timestamp, author = struct.unpack("<L8s", data[8:20])
+(sections,) = struct.unpack("<L", data[20:24])
+
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -35,8 +47,40 @@ if version != VERSION:
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
-
-# We've parsed the magic and version out for you, but you're responsible for
-# the rest of the header and the actual FPFF body. Good luck!
+print("TIMESTAMP: %d" % int(timestamp))
+print("AUTHOR: %s" % str(author))
+print("SECTIONS: %d" % int(sections))
 
 print("-------  BODY  -------")
+offset = 24
+
+for i in range(int(sections)):
+    section_type, section_len = struct.unpack("<LL", data[offset:offset+8])
+    section_len = int(section_len)
+
+    if section_type == SECTION_ASCII or section_type == SECTION_UTF8:
+        (output,) = struct.unpack(("<%ds" % section_len), data[offset + 8: (offset + 8 + section_len)])
+        output = output.decode('utf-8' if section_type == SECTION_UTF8 else 'ascii')
+        print(section_to_name_map[section_type] + " OUTPUT: %s" % (output))
+        
+    elif section_type == SECTION_WORDS or section_type == SECTION_DWORDS or section_type == SECTION_DOUBLES:
+        size = int(section_len/(4 if section_type == SECTION_WORDS else 8))
+        (output,) = struct.unpack(("<%s" % ('L' * size)), data[offset + 8: (offset + 8 + section_len)])
+        print(section_to_name_map[section_type] + " OUTPUT: %s" % (output))
+
+    elif section_type == SECTION_COORD:
+        output = struct.unpack("<dd", data[offset + 8: (offset + 8 + section_len)])
+       	print("COORDS OUTPUT: %s" % str(output))
+
+    elif section_type == SECTION_REFERENCE:
+        output = struct.unpack("<L", data[offset + 8: (offset + 8 + section_len)])
+        print("REFRENCE OUTPUT: %d" % output[0])
+            
+    elif section_type == SECTION_PNG or section_type == SECTION_GIF87 or section_type == SECTION_GIF89:
+        output = struct.unpack(("<%s" % ('B' * section_len)), data[offset + 8: (offset + 8 + section_len)])
+        im_data = image_format_offset_table[section_type] + list(output)
+        file = open("output" + (".png" if section_type == SECTION_PNG else ".gif"), "wb")
+        file.write(bytearray(im_data))
+        print(section_to_name_map[section_type] + " CREATED")
+
+    offset += section_len + 8
--- week/9/writeup/README.md
+++ week/9/writeup/README.md
@@ -1,15 +1,46 @@
 # Writeup 9 - Forensics II
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Justin Becker
+Section: 0101
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Justin T. Becker
 
 
 ## Assignment details
 
 ### Part 1 (45 Pts)
 
+1. Warmup: what IP address has been attacked?
+
+142.93.136.81
+
+2. What kind of assessment tool(s) were the attackers using against the victim machine? List the name(s) of the tool(s) as well.
+
+nmap
+
+3. What are the hackers' IP addresses, and where are they connecting from?
+
+159.203.113.181. This is a digitalocean droplet, so we can't determine where the attackers are located with any certainty without a subpeona on Digitalocean's records.
+
+4. What port are they using to steal files on the server?
+
+21
+
+5. Which file did they steal? What kind of file is it? Do you recognize the file?
+
+find_me.jpg. It's an image file. exiftools reveals that it was taken at Los Dedos de Punta del Este in Maldonado, Uruguay
+
+6. Which file did the attackers leave behind on the server?
+
+They left behind greetz.fpff
+
+7. What is a countermeasure to prevent this kind of intrusion from happening again? Note: disabling the vulnerable service is *not* an option.
+
+Using a firewall to prevent hostile actors from nmapping your service. Additionally, serving your FTP service on a non-standard port number could reduce the chances of a hostile actor discovering the service.
+
 ### Part 2 (55 Pts)
+
+CMSC389R-{w3lc0me_b@ck_fr0m_spr1ng_br3ak}
+CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}
