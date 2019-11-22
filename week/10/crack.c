6769eb8257cc79e65184550678b8292e8db73e9b
Justin Becker <geekman7473@gmail.com>
2019-11-21 22:58:50 -0500

    Fixed compile time issues

--- week/10/crack.c
+++ week/10/crack.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include <openssl/rand.h>
+
 #include "crypto.h"
 #include "common.h"
 
@@ -19,20 +21,21 @@ int main(int argc, char **argv) {
 
     if (stat(LEDGER_FILE, &st) == 0) {
         unsigned char fd_key_hash[16], generated_key_hash[16], password_char[17];
+        unsigned char *key, *key_hash;
         int ctext_len = st.st_size - 48, ptext_len, i;
         int password_guess = 0;
         int fd;
         fd = open(LEDGER_FILE, O_RDONLY, PERMISSIONS);
 
         read(fd, fd_key_hash, 16);
+        memset(&password_char, 0, 17);
 
         do {
-            memset(&password_char, 0, 33);
-            unsigned char *key, *key_hash;
-            itoa(password_guess, password_char, 16);
-            key = md5_hash(password_char, strlen(password_char));
+            RAND_bytes(password_char, 16);
+            key = md5_hash(password_char, 16);
             memset(key+2, 0, 14);
             key_hash = md5_hash(key,2);
+            printf("%x\n", key_hash);
         } while (memcmp(fd_key_hash, key_hash, 16) != 0);
 
         printf("%s", password_char);
